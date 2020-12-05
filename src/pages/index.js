98b6a029a7fc92370a9c896032d5b56c4db2215e
florianschiesterl <florian.schiesterl@me.com>
2020-12-05 17:13:23 +0100

    First commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -72,7 +72,7 @@ const IndexPage = () => {
     <main style={pageStyles}>
       <title>Home Page</title>
       <h1 style={headingStyles}>
-        Congratulations
+        Congratulations Flo
         <br />
         <span style={headingAccentStyles}>— you just made a Gatsby site!</span>
         <span role="img" aria-label="Party popper emojis">
