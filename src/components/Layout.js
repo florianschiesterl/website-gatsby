ff6e563844b715c06313c9fbdc835fba85ae215c
florianschiesterl <florian.schiesterl@me.com>
2020-12-13 12:49:15 +0100

    Optimize for mobile

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,7 +11,7 @@ function Layout(props) {
       <title>Florian Schiesterl - Colorful within</title>
 
       <div className="h-screen">
-        <section className="bg-black dark:bg-white w-1/4 h-full p-16 xl:p-20 pr-4 fixed spring transition-colors duration-1000">
+        <section className="bg-black dark:bg-white lg:w-1/4 lg:h-full p-16 xl:p-20 pr-4 lg:fixed spring transition-colors duration-1000">
           <nav>
             <Link to="/">
               <h1 className={classesText}>Florian Schiesterl</h1>
@@ -32,7 +32,7 @@ function Layout(props) {
               }}
             ></button>
           </nav>
-          <nav className="bottom-16 left-16 xl:bottom-20 xl:left-20 absolute">
+          <nav className="lg:bottom-16 lg:left-16 xl:bottom-20 xl:left-20 lg:absolute">
             <Navitem title="Odyssey East" link="/odyssey" />
             <Navitem title="About" link="/about" />
             <Navitem
@@ -43,8 +43,10 @@ function Layout(props) {
           </nav>
         </section>
 
-        <section className="bg-black dark:bg-white ml-1/4 w-3/4 spring transition-colors duration-1000">
-          <div className="p-16 xl:p-20 animate-fadein">{props.children}</div>
+        <section className="bg-black dark:bg-white lg:ml-1/4 lg:w-3/4 spring transition-colors duration-1000">
+          <div className="p-4 lg:p-16 xl:p-20 animate-fadein">
+            {props.children}
+          </div>
         </section>
       </div>
     </main>
