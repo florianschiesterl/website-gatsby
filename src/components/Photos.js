b9f4a1dfdd46b8637d7c01bbef77a734ad78fca6
florianschiesterl <florian.schiesterl@me.com>
2021-05-24 00:17:46 +0200

    Add shuffling

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,10 +1,23 @@
 import React from "react";
 import Img from "gatsby-image";
 
+function shuffleArray(array) {
+  let i = array.length - 1;
+  for (; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
 function Photos(props) {
+  const shuffledPosts = shuffleArray(props.data.images.nodes);
+
   return (
     <div>
-      {props.data.images.nodes.map((image) => (
+      {shuffledPosts.map((image) => (
         <Img
           className="mb-16 md:mb-32 rounded"
           key={image.id}
